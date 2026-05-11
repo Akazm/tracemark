d20d683b1ab3ef486aed14efab0d549945285862
Andre Kazmierczak <a.kazm@icloud.com>
2026-05-11 11:23:23 +0200

    fix: correctly include privacy-policy.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === "production";
+
   return {
     build: {
       sourcemap: isProduction ? "hidden" : true,
+      rollupOptions: {
+        input: {
+          // 'main' is your primary entry point
+          main: resolve(__dirname, "index.html"),
+          // Add your extra pages here
+          privacy: resolve(__dirname, "privacy-policy.html"),
+        },
+      },
     },
     base: "./",
   };
